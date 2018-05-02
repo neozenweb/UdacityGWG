9b61a49165db7dc51efd79d0e7dd98ce3fcc82b9
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-05-02 09:11:48 -0400

    Add files via upload

--- javascript/portfoliojs.js
+++ javascript/portfoliojs.js
@@ -1,35 +1,14 @@
 // JavaScript Document
-
-function opensite(imgnm){
-	
-	imgnm = imgnm.substring(imgnm.indexOf("images/")+7,imgnm.length -4);
-	
-	var linkurls =["http://moviestylehomeshopping.com/",
-				   "http://www.forsythyouthorchestra.com/",
-				   "http://rawgit.com/neozenweb/CodingChallenge/master/PixelArt.html",
-				   "http://rawgit.com/neozenweb/CodingChallenge/master/Invitation.html",
-				   "http://rawgit.com/neozenweb/CodingChallenge/master/TouristAttractions.html"
-				   ];
-	
-	
-	for(var ct=0;ct<linkurls.length;ct++)
-	{
-		
-		if(linkurls[ct].toLowerCase().indexOf(imgnm.toLowerCase()) > 0)
-		   break;
-		
-			
+function opensite(imgnm) {
+	imgnm = imgnm.substring(imgnm.indexOf("images/") + 7, imgnm.length - 4);
+	var linkurls = ["http://moviestylehomeshopping.com/", "http://www.forsythyouthorchestra.com/", "http://rawgit.com/neozenweb/CodingChallenge/master/PixelArt.html", "http://rawgit.com/neozenweb/CodingChallenge/master/Invitation.html", "http://rawgit.com/neozenweb/CodingChallenge/master/TouristAttractions.html"];
+	for (var ct = 0; ct < linkurls.length; ct++) {
+		if (linkurls[ct].toLowerCase().indexOf(imgnm.toLowerCase()) > 0) break;
 	}
-	
-	var newone = window.open(linkurls[ct],  "width=200, height=100");
+	var newone = window.open(linkurls[ct], "width=200, height=100");
 }
-function dispscr(imgsr,ctr)
-{
-	
+
+function dispscr(imgsr, ctr) {
 	var ct = parseInt(ctr) - 1;
-	
-	
-	
-	document.getElementById("mainimg").setAttribute("src",imgsr);
-	
+	document.getElementById("mainimg").setAttribute("src", imgsr);
 }
